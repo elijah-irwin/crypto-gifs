3d251b875b0fcc1b8c692b5afee90a0a71719b39
Mckenzie Irwin <irwinmck@gmail.com>
2022-03-12 23:03:30 -0500

    ternary

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -6,17 +6,16 @@ import { useTransactionContext } from '../context/TransactionContext';
 // Components
 import TransactionCard from './TransactionCard';
 
-// Utils
-import { tempData } from '../utils/temp-data';
-
 const Transactions = () => {
-  const { transactions } = useTransactionContext();
+  const { wallet, transactions } = useTransactionContext();
 
   return (
     <div className=' flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions'>
       <div className='flex flex-col md:p-12 py-12 px-4'>
         <h3 className='text-white text-3xl text-center my-2'>
-          See The Latest Transactions
+          {wallet
+            ? 'Latest Transactions'
+            : 'Connect With Metamast To See The Latest Transactions'}
         </h3>
 
         <div className='flex flex-wrap justify-center items-center mt-10'>
